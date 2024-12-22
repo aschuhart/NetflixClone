a1d97a8a61d55a6dfd2f4479f7124ab76eeb0ddb
Alex Schuhart <alexschuhart@gmail.com>
2024-12-22 17:10:34 -0500

    Fixed but new problem of background image not loading

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,37 +1,44 @@
-import React, {useState} from 'react'
-import './Login.css'
-import logo from '../../assets/logo.png'
+import React, { useState } from 'react';
+import './Login.css';
+import logo from '../../assets/logo.png';
 
 const Login = () => {
+  const [signState, setSignState] = useState("Sign In");
 
-const [signState, setSignState] = useState("Sign In")
-
+  const handleStateSwitch = () => {
+    setSignState(prevState => (prevState === "Sign In" ? "Sign Up" : "Sign In"));
+  };
 
   return (
     <div className='Login'>
-      <img src={logo} className='login-logo' alt="" />
+      <img src={logo} className='login-logo' alt="Logo" />
       <div className="login-form">
         <h1>{signState}</h1>
         <form>
-          <input type="text" placeholder='Your name' />
+          {/* Conditionally render input field for "Your name" */}
+          {signState === "Sign Up" && <input type="text" placeholder='Your name' />}
           <input type="email" placeholder='Email' />
           <input type="password" placeholder='Password' />
-          <button>Sign Up</button>
+          <button type="button">{signState === "Sign In" ? "Sign In" : "Sign Up"}</button>
           <div className="form-help">
             <div className="remember">
-              <input type="checkbox" />
-              <label htmlFor="">Remember Me</label>
+              <input type="checkbox" id="rememberMe" />
+              <label htmlFor="rememberMe">Remember Me</label>
             </div>
             <p>Need Help?</p>
           </div>
         </form>
         <div className="form-switch">
-          <p>New to Netflix? <span>Sign Up Now</span></p>
-          <p>Already have an account? <span>Sign In Now</span></p>
+          {signState === "Sign In" ? (
+            <p>New to Netflix? <span onClick={handleStateSwitch}>Sign Up Now</span></p>
+          ) : (
+            <p>Already have an account? <span onClick={handleStateSwitch}>Sign In Now</span></p>
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+
+export default Login;
